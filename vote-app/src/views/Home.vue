a1433c02d2090b704b688050c813aa9ef68e2618
Terje Topland <terjetopland@gmail.com>
2020-12-24 13:22:49 +0100

    prøvd å flytte teksten under bildet

--- vote-app/src/views/Home.vue
+++ vote-app/src/views/Home.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="home">
-    <img width="900" height="450" alt="Doors img." src="../pic/doorsHome.jpg">;
-
+    <img width="900" height="450" alt="hei" src="../pic/doorsHome.jpg">
+  
     <span id="createrOfPic" class="caption">
       Image by <a href="https://pixabay.com/users/qimono-1962238/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1767563">Arek Socha</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1767563">Pixabay</a>
-    </span>;
+    </span>
     <!--vil flytte teksten til venstre side under bildet-->
 
     <HelloWorld msg="Vote-app"/>
