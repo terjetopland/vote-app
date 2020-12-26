2d75814aa440e870e152d3cde41feabbd06e58ba
Terje Topland <terjetopland@gmail.com>
2020-12-26 13:22:04 +0100

    tekst er nå under bildet...men

--- vote-app/src/views/Home.vue
+++ vote-app/src/views/Home.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="home">
-    <img width="900" height="450" alt="hei" src="../pic/doorsHome.jpg">
-  
-    <span id="createrOfPic" class="caption">
-      Image by <a href="https://pixabay.com/users/qimono-1962238/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1767563">Arek Socha</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1767563">Pixabay</a>
-    </span>
+    <div class="item">
+      <img width="600" height="300" alt="pictureOfDoor" src="../pic/doorsHome.jpg">
+        <p id="picDoor"> Image by <a href="https://pixabay.com/users/qimono-1962238/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1767563">Arek Socha</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1767563">Pixabay</a>
+        </p>
+    </div>
     <!--vil flytte teksten til venstre side under bildet-->
 
     <HelloWorld msg="Vote-app"/>
   </div>
 </template>
+
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
